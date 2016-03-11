7d4fcaa08df8dd0a35245c4163db67096d67b46f
Anko Anchev <anko.anchev@gmail.com>
2016-03-11 15:41:04 +0000

    Added in Utility scripts

---
+++ Assets/Scripts/Utilities/CoroutineExtensions.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class CoroutineExtensions
+{
+    public delegate void Action();
+    public delegate object Expression();
+
+
+    public static IEnumerator Then(this IEnumerator self, IEnumerator then)
+    {
+        while (self.MoveNext())
+            yield return self.Current;
+
+        while (then.MoveNext())
+            yield return then.Current;
+    }
+
+    public static IEnumerator Then(this IEnumerator self, Action then)
+    {
+        return self.Then(() => { then(); return null; });
+    }
+
+    public static IEnumerator Then(this IEnumerator self, Expression then)
+    {
+        while (self.MoveNext())
+            yield return self.Current;
+
+        yield return then();
+    }
+
+
+    public static IEnumerator ButFirst(this IEnumerator self, IEnumerator first)
+    {
+        while (first.MoveNext())
+            yield return first.Current;
+
+        while (self.MoveNext())
+            yield return self.Current;
+    }
+
+    public static IEnumerator ButFirst(this IEnumerator self, Action first)
+    {
+        return self.ButFirst(() => { first(); return null; });
+    }
+
+    public static IEnumerator ButFirst(this IEnumerator self, Expression first)
+    {
+        yield return first();
+
+        while (self.MoveNext())
+            yield return self.Current;
+    }
+
+
+    public static IEnumerator Then(this IEnumerator self, YieldInstruction then)
+    {
+        while (self.MoveNext())
+            yield return self.Current;
+
+        yield return then;
+    }
+
+    public static IEnumerator Then(this YieldInstruction self, IEnumerator then)
+    {
+        yield return self;
+
+        while (then.MoveNext())
+            yield return then.Current;
+    }
+
+
+    public static IEnumerator Then(this YieldInstruction self, YieldInstruction then)
+    {
+        yield return self;
+        yield return then;
+    }
+
+    public static IEnumerator Then(this YieldInstruction self, Action then)
+    {
+        return self.Then(() => { then(); return null; });
+    }
+
+    public static IEnumerator Then(this YieldInstruction self, Expression then)
+    {
+        yield return self;
+        yield return then();
+    }
+
+    
+    public static Coroutine Start(this IEnumerator coroutine, MonoBehaviour script)
+    {
+        return script.StartCoroutine(coroutine);
+    }
+
+    public static void Stop(this Coroutine coroutine, MonoBehaviour script)
+    {
+        script.StopCoroutine(coroutine);
+    }
+}
---
+++ Assets/Scripts/Utilities/FadeExtensions.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+
+public static class FadeExtensions
+{
+    public static float defaultFadeDuration = 1f; //seconds
+
+    public static float GetOpacity(this GameObject gameObject)
+    {
+        try {
+            SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();
+            return renderer.color.a;
+        }
+        catch (Exception) {
+            return -1;
+        }
+    }
+
+    public static bool SetOpacity(this GameObject gameObject, float opacity)
+    {
+        try {
+            SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();
+            Color color = renderer.color;
+            color.a = opacity;
+            renderer.color = color;
+
+            return true;
+        }
+        catch (Exception) {
+            return false;
+        }
+    }
+
+
+    public static IEnumerator FadeOut(this GameObject gameObject)
+    {
+        return gameObject.FadeOut(defaultFadeDuration);
+    }
+
+    public static IEnumerator FadeOut(this GameObject gameObject, float duration)
+    {
+        float start = gameObject.GetOpacity();
+        float end = 0;
+        float diff = end - start;
+
+        return new Animation(duration,
+            p => gameObject.SetOpacity(start + p * diff))
+            .Then(() => gameObject.SetOpacity(end));
+    }
+
+
+    public static IEnumerator FadeIn(this GameObject gameObject)
+    {
+        return gameObject.FadeIn(defaultFadeDuration);
+    }
+
+    public static IEnumerator FadeIn(this GameObject gameObject, float duration)
+    {
+        float start = gameObject.GetOpacity();
+        float end = 1;
+        float diff = end - start;
+
+        return new Animation(duration,
+            p => gameObject.SetOpacity(start + p * diff))
+            .Then(() => gameObject.SetOpacity(end));
+    }
+}

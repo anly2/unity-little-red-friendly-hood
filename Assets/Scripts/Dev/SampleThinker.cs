2bf2f34cc638697cc3798265714fec041e743b15
Anko Anchev <anko.anchev@gmail.com>
2016-03-30 15:39:37 +0100

    Thought Bubble sample usage (in test scene)

---
+++ Assets/Scripts/Dev/SampleThinker.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SampleThinker : MonoBehaviour
+{
+
+    public string thought;
+
+    void Start()
+    {
+        gameObject.Think(thought);
+    }
+}

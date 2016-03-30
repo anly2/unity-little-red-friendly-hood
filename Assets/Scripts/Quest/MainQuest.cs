c94bf66c88a36c90a4441520d52f0f657bb2eb5f
Anko Anchev <anko.anchev@gmail.com>
2016-03-30 23:52:13 +0100

    Main Quest started anew

--- Assets/Scripts/Quest/MainQuest.cs
+++ Assets/Scripts/Quest/MainQuest.cs
@@ -3,27 +3,37 @@ using System.Collections;
 
 public class MainQuest : Quest {
 
-	internal protected MainQuest() : base("Main")
+    private GameObject player;
+
+	void Start()
     {
+        name("Main");
         description("The Quest defining the main plot of the game.");
         startsActive(true);
 
-        GameObject wolf = null; //#!
-        int friendship = 0;
+
+        GameObject mother = GameObject.Find("Mother");
 
         state("S0")
-            .onEnter((s) => { /* play Intro */ });
+            .onEnter(s =>
+            {
+                player = GameObject.FindWithTag("Player");
+                GameObject spawn = GameObject.FindWithTag("Respawn");
 
-        state("S1").conversation().with(wolf)
-            .they("What are you carrying under the apron?")
-            .player("Yesterday was baking day, so I have cake and wine for...")
-            .they("It smells delicious.")
-            .player().choice()
-                .option("Thank you.", "S2")
-                .option("Thank you! Would you like some?",
-                        (c)=>{ friendship++; c.getQuest().enter("S2"); });
+                player.transform.position = spawn.transform.position;
+            })
 
-        //defaults:
-        //conv.activatedBy(Activators.InRange(player, wolf, 5f))
+            .scene()
+                .actor(mother)
+                    .act(aq => mother.FadeIn(0.5f), 0.5f)
+                    .delay(0.5f)
+                    .say("Come, Little Red-Cap, here is a piece of cake and a bottle of wine; take them to your grandmother, she is ill and weak, and they will do her good.")
+                    .say("Set out before it gets hot, and when you are going, walk nicely and quietly and do not run off the path, or you may fall and break the bottle, and then your grandmother will get nothing.")
+                    .say("And when you go into her room, don't forget to say, \"Good morning\", and don't peep into every corner before you do it.")
+                .actor(player)
+                    .say("I will take great care.")
+                .actor(mother)
+                    .act(aq => mother.FadeOut(0.5f))
+                .transition("S1");
     }
 }

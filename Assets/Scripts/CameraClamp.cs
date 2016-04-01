da5c8bb3aed596d330ea5514050e12de3f035b88
Anko Anchev <anko.anchev@gmail.com>
2016-04-01 23:16:14 +0100

    Camera Clamp
    + Added a CameraClamp script which does not allow the camera it is
    attached to to go out of the bounds it has set.
    + Created a 'Camera Bounding Box' component in Scene1 and used the
    CameraClamp script with it.

---
+++ Assets/Scripts/CameraClamp.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System;
+
+public class CameraClamp : MonoBehaviour {
+
+    public GameObject boundingBox;
+
+    private Camera cam;
+    private Bounds bounds;
+    private Vector3 offset;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+
+        if (cam == null)
+            throw new InvalidOperationException("The CameraClamp component must be attached to a Camera.");
+
+        bounds = GetBounds(boundingBox);
+    }
+
+    public static Bounds GetBounds(GameObject o)
+    {
+        Collider c = o.GetComponent<Collider>();
+        if (c != null)
+            return c.bounds;
+
+        Renderer r = o.GetComponent<Renderer>();
+        if (r != null)
+            return r.bounds;
+
+        return new Bounds(o.transform.position, o.transform.localScale);
+    }
+	
+
+	void Update () {
+        transform.Translate(-offset);
+        offset = GetExcess();
+        transform.Translate(offset);
+	}
+
+
+    private static Vector3 V0 = new Vector3(0, 0, 0);
+    Vector3 GetExcess()
+    {
+        if (bounds.Contains(cam.transform.position))
+            return V0;
+
+        return bounds.ClosestPoint(cam.transform.position) - cam.transform.position;
+    }
+}

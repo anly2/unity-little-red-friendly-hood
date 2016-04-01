42d1e870d529b491275363e22bfdb2569ee43b83
Anko Anchev <anko.anchev@gmail.com>
2016-04-01 20:51:35 +0100

    Main Quest: a tiny push forward

--- Assets/Scripts/Quest/MainQuest.cs
+++ Assets/Scripts/Quest/MainQuest.cs
@@ -12,21 +12,21 @@ public class MainQuest : Quest {
         startsActive(true);
 
 
+        player = GameObject.FindWithTag("Player");
         GameObject mother = GameObject.Find("Mother");
+        GameObject wolf = GameObject.Find("Wolf");
+
 
         state("S0")
             .onEnter(s =>
             {
-                player = GameObject.FindWithTag("Player");
                 GameObject spawn = GameObject.FindWithTag("Respawn");
-
                 player.transform.position = spawn.transform.position;
             })
 
             .scene()
                 .actor(mother)
-                    .act(aq => mother.FadeIn(0.5f), 0.5f)
-                    .delay(0.5f)
+                    .wait(1.5f)
                     .say("Come, Little Red-Cap, here is a piece of cake and a bottle of wine; take them to your grandmother, she is ill and weak, and they will do her good.")
                     .say("Set out before it gets hot, and when you are going, walk nicely and quietly and do not run off the path, or you may fall and break the bottle, and then your grandmother will get nothing.")
                     .say("And when you go into her room, don't forget to say, \"Good morning\", and don't peep into every corner before you do it.")
@@ -34,6 +34,20 @@ public class MainQuest : Quest {
                     .say("I will take great care.")
                 .actor(mother)
                     .act(aq => mother.FadeOut(0.5f))
+            .scene()
+                .activatedBy(Activators.InRange(player, wolf, 4f))
                 .transition("S1");
+
+        state("S1")
+            .scene()
+                .actor(wolf)
+                    .act((aq) =>
+                        aq.getQuest().StartCoroutine(
+                            wolf.MotionTo(
+                                Vector3.MoveTowards(
+                                    wolf.transform.position,
+                                    player.transform.position, 2f),
+                                1f)), 1f)
+                    .say("Hello");
     }
 }

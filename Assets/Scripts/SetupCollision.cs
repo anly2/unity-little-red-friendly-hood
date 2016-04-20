85a336329025e6fc4a56d848ab83c4de53d8e25c
ta2g12 <ta2g12@soton.ac.uk>
2016-04-20 10:23:34 +0100

    MessageAPI added with trigger on woods script to show usage

---
+++ Assets/Scripts/SetupCollision.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetupCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Physics2D.IgnoreLayerCollision (8, 9);
+	}
+}

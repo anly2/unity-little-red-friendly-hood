61e9833fb270fe3fc50f9c63c0a896f7f4ab5854
ta2g12 <ta2g12@soton.ac.uk>
2016-04-21 15:02:58 +0100

    Alignment sorted, thought of spacebar jump added

--- Assets/Scripts/StartChallenge.cs
+++ Assets/Scripts/StartChallenge.cs
@@ -11,6 +11,7 @@ public class StartChallenge : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D c) {
 		if (c.gameObject.tag.Equals ("Player") == true) { 
+			player.Think("I think I can jump [Spacebar] between these stepping stones");
 			challenge = trigger.GetComponent<SteppingStonesChallenge> ();
 			pm = challenge.player.GetComponent<PlayerMovement> ();
 			challenge.inPosition = true;

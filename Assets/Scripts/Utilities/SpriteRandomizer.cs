4c5ccecb711e25d4d01789fb57f88a036d6deb05
Anko Anchev <anko.anchev@gmail.com>
2016-04-21 11:54:44 +0100

    Added a Sprite Randomizer Utility class

---
+++ Assets/Scripts/Utilities/SpriteRandomizer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SpriteRandomizer : MonoBehaviour {
+
+    public List<Sprite> sprites;
+
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Count)];
+    }
+}

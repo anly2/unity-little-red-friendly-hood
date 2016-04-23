ae538d239b9fb024332d0f5ec13666741f5ec290
Anko Anchev <anko.anchev@gmail.com>
2016-04-23 01:40:23 +0100

    Redo PlayerMovement roc

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,6 @@ public class PlayerMovement : Movement {
     
     private Rigidbody2D rb;
 
-	public bool unRestricted = true;
-
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -15,13 +13,14 @@ public class PlayerMovement : Movement {
 
     void Update ()
     {
-		if (unRestricted) {
-			float moveHorizontal = Input.GetAxis ("Horizontal");
-			float moveVertical = Input.GetAxis ("Vertical");
+        if (speed == 0)
+            return;
+        
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
 
-			Vector3 movement = new Vector3 (moveHorizontal, moveVertical);
+		Vector3 movement = new Vector3 (moveHorizontal, moveVertical);
 
-			rb.velocity = movement * speed;
-		}
+		rb.velocity = movement * speed;
     }
 }

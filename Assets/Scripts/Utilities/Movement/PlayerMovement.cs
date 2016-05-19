ee365b597fc27880e21ca6d316ad23daa0dc789a
Anko Anchev <anko.anchev@gmail.com>
2016-05-19 22:35:41 +0100

    Updated the PlayerMovement script

--- Assets/Scripts/Utilities/Movement/PlayerMovement.cs
+++ Assets/Scripts/Utilities/Movement/PlayerMovement.cs
@@ -4,23 +4,34 @@ using System.Collections;
 public class PlayerMovement : Movement {
     
     private Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer rend;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        rend = GetComponent<SpriteRenderer>();
     }
 
 
     void Update ()
     {
         if (speed == 0)
+        {
+            animator.SetBool("Moving", false);
             return;
+        }
         
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
-		Vector3 movement = new Vector3 (moveHorizontal, moveVertical);
+        animator.SetBool("Moving", (moveHorizontal != 0 || moveVertical != 0));
 
+        if (rend != null)
+            rend.flipX = (moveHorizontal < 0);
+
+		Vector3 movement = new Vector3 (moveHorizontal, moveVertical);
 		rb.velocity = movement * speed;
     }
 }

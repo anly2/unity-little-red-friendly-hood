4c42fdf485b0da3fd58e3984eccd6228b5e7bba5
Anko Anchev <anko.anchev@gmail.com>
2016-05-21 18:14:22 +0100

    Added RiverLedge Jump tooltip

---
+++ Assets/Scripts/LedgeJumpHint.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LedgeJumpHint : MonoBehaviour {
+
+    private bool shownAlready = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (shownAlready) return;
+
+        if (other.tag != "Player")
+            return;
+
+        other.gameObject.Think("I should be able to jump [Spacebar] over those stones.");
+        shownAlready = true;
+    }
+}

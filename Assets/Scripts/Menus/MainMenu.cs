89878c5c1ef70bac3e97eb38a3ce0ab3db712b2c
Anko Anchev <anko.anchev@gmail.com>
2016-05-22 17:13:30 +0100

    Added MainMenu and linked the rest

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -1,4 +1,38 @@
 ﻿using UnityEngine;
 
 public class MainMenu : Menu {
+
+    public void NewGame()
+    {
+        SaveManager.Reload();
+    }
+
+    public void LoadLast()
+    {
+        //#!! checks
+        SaveManager.Load(SaveManager.GetSaves()[0].name);
+    }
+    
+
+    public void SaveGame()
+    {
+        base.Hide();
+        var m = Menus.Get<SaveMenu>();
+        m.referrer = this;
+        m.Show();
+    }
+
+    public void LoadGame()
+    {
+        base.Hide();
+        var m = Menus.Get<LoadMenu>();
+        m.referrer = this;
+        m.Show();
+    }
+
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
 }

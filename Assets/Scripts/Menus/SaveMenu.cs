16804ac4559c8df97e9ed645bcd78846442f0ce4
Anko Anchev <anko.anchev@gmail.com>
2016-05-22 17:11:56 +0100

    Added Save Menu script

---
+++ Assets/Scripts/Menus/SaveMenu.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveMenu : Menu {
+
+    [HideInInspector]
+    public Menu referrer = null;
+
+    public GameObject saveViewPrefab;
+    public GameObject saveList;
+
+    public Text saveName;
+
+
+    public void MakeSave()
+    {
+        base.Hide(true);
+
+        float ts = Time.timeScale;
+        new WaitForSeconds(0)
+            .Then(() => SaveManager.Save(saveName.text))
+            .Then(() => Time.timeScale = ts)
+            .Then(() => Back())
+            .Start(this);
+    }
+
+
+
+    public override void Hide(bool doUnpause = true)
+    {
+        base.Hide(doUnpause);
+        referrer = null;
+    }
+
+
+    public void Back()
+    {
+        if (referrer == null)
+            return;
+        
+        base.Hide();
+        referrer.Show();
+    }
+}
